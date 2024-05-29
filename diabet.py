e20e15f7308234a88da1a6f24cf6bb0a2ff4fbdc
Sicakyuz <cigdem.sicakyuz@gmail.com>
2024-05-29 10:37:18 +0300

    file reading error corrected

--- diabet.py
+++ diabet.py
@@ -56,8 +56,7 @@ pd.set_option('display.width', None)
 pd.set_option('display.max_rows', 20)
 pd.set_option('display.float_format', lambda x: '%.3f' % x)
 
-df = pd.read_csv("diabet.py")
-
+df = pd.read_csv("/Users/asmir/Desktop/MyProjects/DiabetPredicton/diabetes.csv", sep=',')
 
 # ### 2. **Exploratory Data Analysis (EDA):**
 #
